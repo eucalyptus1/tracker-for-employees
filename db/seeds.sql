353219f5d8edba1e447b9dd635022649d31b0e56
euc1 <jamesharrington585@hotmail.com>
2022-04-26 19:59:46 -0400

    populating sql files

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (name)
+VALUES
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Joey Jo-Jo ', 'Junior Shabadoo', 1),
+  ('John Jacob', 'Jingleheimer Schmidt', 1),
+  ('Horatio', 'Hufnagel', 0),
+  ('Zaphod', 'Beeblebrox', 1),
+  ('Rudi', 'Van DiSarzio', 0),
+  ('Spider', 'Dijon', 1),
+  ('Engelbert', 'Humperdink', 1),
+  ('James', 'Van Der Beek', 1),
